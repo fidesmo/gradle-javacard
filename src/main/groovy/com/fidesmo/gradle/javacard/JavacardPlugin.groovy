0f17ada581fe6ff4a80c9bfc7953126a912998b2
Yves Müller <yves.mueller@fidesmo.com>
2014-08-13 11:09:46 +0200

    Factor out ConvertJavacard task
    - introduce reusable task class
    - enable conventions mapping from JavacardExtension to task

--- src/main/groovy/com/fidesmo/gradle/javacard/JavacardPlugin.groovy
+++ src/main/groovy/com/fidesmo/gradle/javacard/JavacardPlugin.groovy
@@ -20,7 +20,7 @@ package com.fidesmo.gradle.javacard
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.tasks.Exec
+import com.fidesmo.gradle.javacard.ConvertJavacardTask
 
 class JavacardPlugin implements Plugin<Project> {
 
@@ -60,55 +60,17 @@ class JavacardPlugin implements Plugin<Project> {
 
     private def addConvertTask(Project project, JavacardExtension jcExtension) {
 
-        def convert = project.getTasks().create("convertJavaCard") {
+        def convert = project.getTasks().create("convertJavaCard", ConvertJavacardTask)
+
+        convert.configure {
             group = 'build'
-            description = 'Create a cap for installation on a smart card'
+            description = 'Create a CAP file for installation on a smart card'
             dependsOn(project.compileJava)
-
-            ext.executableModules = [ '0xd2:0x76:0x00:0x00:0x85:0x01:0x01': 'de.spline.uves.ndef.Ndef' ]
-            ext.packagePath = 'de.spline.uves.ndef'
-            ext.aid = '0xd2:0x76:0x00:0x00:0x85:0x01'
-            ext.version = '1.0'
-            ext.javaCardDirectory = new File(project.getBuildDir(), 'javacard')
-
-            def packageFilepath = packagePath.replace('.', File.separator)
-            def packageName = "ndef" // FIXME:
-
-            ext.capFile = "${javaCardDirectory}/${packageFilepath}/javaCard/${packageName}.cap"
-            ext.extFile = "${javaCardDirectory}/${packageFilepath}/javaCard/${packageName}.ext"
-            ext.classesDir = project.sourceSets.main.output.classesDir
-
-            inputs.dir classesDir
-            outputs.file capFile
-            outputs.file extFile
-
-            doLast {
-                ant.taskdef(name: 'convert',
-                            classname: 'com.sun.javacard.ant.tasks.ConverterTask',
-                            classpath: project.configurations.javaCardTools.asPath)
-
-                ant.convert(CAP: true,
-                            EXP: true,
-                            packagename: packagePath,
-                            packageaid: aid,
-                            majorminorversion: version,
-                            debug: true,
-                            classdir: classesDir,
-                            outputdirectory: new File(project.getBuildDir(), 'javacard'),
-                            exportpath: "${javaCardHome}/api_export_files",
-                            classpath: project.configurations.javaCardTools.asPath) {
-
-                    executableModules.each() { emAid, fqImplementorClass ->
-                        AppletNameAID(appletname: fqImplementorClass, aid: emAid )
-                    }
-                }
-            }
         }
 
-        // cap.conventionMapping.sourcePackage = { capExtension.sourcePackage }
-        // cap.conventionMapping.aid = { capExtension.aid }
-        // cap.conventionMapping.version = { capExtension.version }
-        // cap.conventionMapping.applets = { capExtension.applets }
-        // cap.conventionMapping.javacardHome = { jcExtension.sdk }
+        convert.conventionMapping.aid = { jcExtension.aid }
+        convert.conventionMapping.packagePath = { jcExtension.sourcePackage }
+        convert.conventionMapping.version = { jcExtension.version }
+        convert.conventionMapping.executableModules = { jcExtension.applets }
     }
 }
